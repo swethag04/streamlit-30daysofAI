7baf5e8bffb29c2b5812b9d0a4857d4ca7ededb4
swethag04 <swethag04@gmail.com>
2026-01-04 12:54:50 -0800

    day 1 challenge

---
+++ app/day1.py
@@ -0,0 +1,24 @@
+# Streamlit 30 days of AI
+# Day 1
+# Connect to Snowflake
+
+import streamlit as st
+
+st.title("30 days of AI")
+st.header("Day 1 - Connect to Snowflak")
+
+# Connect to Snowflake
+try:
+    # Works in Streamlit in Snowflake
+    from snowflake.snowpark.context import get_active_session
+    session = get_active_session()
+except:
+    # Works locally and on Streamlit Community Cloud
+    from snowflake.snowpark import Session
+    session = Session.builder.configs(st.secrets["connections"]["snowflake"]).create()
+
+# Query Snowflake version
+version = session.sql("SELECT CURRENT_VERSION()").collect()[0][0]
+
+# Display results
+st.success(f"Successfully connected! Snowflake Version: {version}")
